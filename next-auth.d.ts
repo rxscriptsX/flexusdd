0eedf9ce37a3285186c1b5d60e5a960a3668be2e
rxscriptsX <contactnotifieralex@gmail.com>
2026-05-03 22:38:51 +0200

    Update next-auth.d.ts

--- next-auth.d.ts
+++ next-auth.d.ts
@@ -3,11 +3,13 @@ import "next-auth";
 declare module "next-auth" {
   interface Session {
     accessToken?: string;
+    userGuilds?: any[];
   }
 }
 
 declare module "next-auth/jwt" {
   interface JWT {
     accessToken?: string;
+    guilds?: any[];
   }
 }
