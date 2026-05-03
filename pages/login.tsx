338aa8306e7eb4ecebf442c4cd8c0162a146e09e
rxscriptsX <contactnotifieralex@gmail.com>
2026-05-03 21:34:36 +0200

    Create login.tsx

---
+++ pages/login.tsx
@@ -0,0 +1,23 @@
+import { signIn, useSession } from "next-auth/react";
+import { useRouter } from "next/router";
+import { useEffect } from "react";
+
+export default function Login() {
+  const { data: session } = useSession();
+  const router = useRouter();
+
+  useEffect(() => {
+    if (session) {
+      router.push("/dashboard");
+    }
+  }, [session, router]);
+
+  return (
+    <div>
+      <h1>Iniciar sesión en FLEXUS Dashboard</h1>
+      <button onClick={() => signIn("discord")}>
+        Iniciar sesión con Discord
+      </button>
+    </div>
+  );
+}
